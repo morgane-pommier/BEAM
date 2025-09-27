e517e5af53122791b1185ce92caa343113e522d9
David Lusseau <31443588+dlusseau@users.noreply.github.com>
2025-09-27 16:33:56 +0200

    Add files via upload

--- lib/annotate_bpue.R
+++ lib/annotate_bpue.R
@@ -5,19 +5,19 @@ annotate_bpue <- function(bpue, cols) {
     
     #Aggregating daysatsea across same levels used in calc_bpue
     all_fishing_effort <- all_fishing_effort[, .(
-        daysatseaf = sum(daysatseaf, na.rm = TRUE)), by = .(ecoregion, metierl4)]
+        daysatseaf = sum(daysatseaf, na.rm = TRUE)), by = cols]
     
     #Retrieve n_ind and daysatsea information (model inputs) from obs3, aggregated at the same level used in calc_bpue
     annotations <- obs3[taxon_bycatch_monitor_ok==TRUE, .(
-        daysatsea = sum(daysatsea, na.rm = TRUE), n_ind = sum(n_ind, na.rm=T)), by = .(species,ecoregion, metierl4)]
+        daysatsea = sum(daysatsea, na.rm = TRUE), n_ind = sum(n_ind, na.rm=T)), by =c("species", cols)]
     
     #Joining annotations to original bpue model
     
-    bpue2 <- copy(bpue1)
+    bpue2 <- copy(bpue)
 
-    bpue2[annotations, on = .(ecoregion, metierl4, species), c("daysatsea", "n_ind") := list(i.daysatsea, i.n_ind)]
+    bpue2[annotations, on = c("species", cols), c("daysatsea", "n_ind") := list(i.daysatsea, i.n_ind)]
     
-    bpue2[all_fishing_effort, on = .(ecoregion, metierl4), daysatseaf := i.daysatseaf]
+    bpue2[all_fishing_effort, on = cols, daysatseaf := i.daysatseaf]
     
     bpue2[,monitoring_coverage := daysatsea / daysatseaf]
     bpue2
