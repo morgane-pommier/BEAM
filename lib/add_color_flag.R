5bc431f747f028c9f0637b3d0ecdb6391b7f8d34
Morgane Pommier <67705024+morgane-pommier@users.noreply.github.com>
2026-01-07 16:40:14 +0100

    Update add_color_flag.R

--- lib/add_color_flag.R
+++ lib/add_color_flag.R
@@ -1,16 +1,16 @@
-#07/01/2026
+#07/01/2026 - MP
 
-#This function adds a combined flag taking into account monitoring coverage and reliability estimations. The flag is the HEX code for a color on a bivariate palette, that can be used for plotting.
-#We also add an additional descriptive column "flag_description" 
-#The function is designed to work on the output from the function "reliability_estimation_p.R", and makes use of the "bpue_estimates" object 
+#This function adds a combined flag taking into account monitoring coverage and reliability estimations. The flag is a color on a bivariate palette, but we also add an additional descriptive column. 
+#The function is designed to work on the output from the function "reliability_estimation_p.R" or "reliability_estimation_p_ready.R", and makes use of the "bpue_estimates" object 
 #It take "cols" as an argument, to recover monitoring coverage from "bpue2" (output from "annotate_bpue.R") at the correct aggregation level. (annotate_bpue.R also takes the cols argument and can be run a different aggregation levels). 
-#Currently, the reliability test function is only implemented at an ecoregion / métier level 4 scale (not métier level 5 or ICES area), so by default, cols = c("ecoregion", "metierl4")
+#By default, cols = c("ecoregion", "metierl4"), but this can be amended to include metier level 5 or area code.
 
 add_color_flag <- function(bpues_estimates, bpue2, cols = c("ecoregion", "metierl4")) {
-  #Recovering the monitoring coverage info annotated in bpue2, at the same level of aggregation ("cols") - most of the time, cols=c("ecoregion", "metierl4"))
   
-  #This first part deals with difference in column names since we're working of an object in which column names were altered for printing. Not very clean, we might want to improve this in the future ?
-  #There might be a faster way of doing this.
+ 
+  
+  #This first part deals with difference in column names in cases where we're working of an object in which column names were altered for printing (e.g. table.print). 
+#Added this because in the markdown, reliability_estimation_p.R is run on bpue_table_print which has capital letters and spaces in column names, but reliability_estimation_p_ready.R to get similar results at area, metier L5 scales is run directly on the bpue1_area objects, output from calc_bpue, where column names are all lowercase. Tried to make it flexible depending on which object is used.
   
   original_colnames <- colnames(bpues_estimates)
   
@@ -21,7 +21,7 @@ add_color_flag <- function(bpues_estimates, bpue2, cols = c("ecoregion", "metier
     `ICES area`  = "areacode" #Not sure how ICES area column is called in table_print for estimates at the area scale. Is it implemented yet ?
   )
   
-  # Compute which of the internal names are requested and present
+  # Determine which of the capitalised bpue_estimates colnames names are requested and present
   requested <- intersect(names(bpues_estimates), names(names_changes))
   to_rename <- requested[names_changes[requested] %in% cols]
   
@@ -31,9 +31,13 @@ add_color_flag <- function(bpues_estimates, bpue2, cols = c("ecoregion", "metier
     setnames(bpues_estimates, old = to_rename, new = unname(names_changes[to_rename]))
   }
   
-  setnames(bpues_estimates, "Species", "species")
+  if ("Species" %in% names(bpues_estimates)) {
+    setnames(bpues_estimates, "Species", "species")
+  }
+
+   #Recovering the monitoring coverage info annotated in bpue2, at the same level of aggregation ("cols") - most of the time, cols=c("ecoregion", "metierl4"))
   
-  bpues_estimates[bpue2,on = c("species", cols),`Monitoring coverage` := i.monitoring_coverage] #This column actually already exists at the moment, just set up the function like this if we ever want to go to different level of aggregations.
+  bpues_estimates[bpue2,on = c("species", cols),`Monitoring coverage` := i.monitoring_coverage] #This column actually already exists if the input object is derived from table_print, but set up the function like this so it can be run wether bpues_estimates is the output of reliability_estimation_p.R (run on table_print format, already contains Monitoring coverage), or reliability_estimation_p_ready.R (run on calc_bpue bpue1 output, does not contain monitoring coverage info)
   
   colnames(bpues_estimates) <- c(original_colnames) # Going back to original names after join
   
@@ -59,5 +63,3 @@ add_color_flag <- function(bpues_estimates, bpue2, cols = c("ecoregion", "metier
   
   bpues_estimates
 }
-
-
